81a19457e3bc463537453fde7c737bdccbcc4ca4
marco maione <maionemarco14@gmail.com>
2022-04-12 15:11:35 +0200

    aggiornamento

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,62 +1,14 @@
 <template>
+    <main>
+        <router-view></router-view>
+    </main>
 
-<div class="container">
-    <h1>  Elenco dei post</h1>
-    <div class="row">
-        <div class="col-4" v-for="post in posts" :key="post.id">
-            <div class="card justify-content-center">
-                <div class="card-body">
-                    <h5 class="card-title">{{post.title}}</h5>
-                    <p class="card-text">{{post.content}}</p>
-                    <a href="#" class="btn btn-primary">Vedi articolo completo</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <nav aria-label="Page navigation example ">
-        <ul class="pagination">
-            <li class="page-item" :class="(currentPage == 1)?'disabled': ''"><span class="page-link" @click="getPosts(currentPage - 1)">Previous</span></li>
-            <li class="page-item" :class="(currentPage == lastPage)?'disabled': ''"><span class="page-link" @click="getPosts(currentPage + 1)">Next</span></li>
-        </ul>
-    </nav>
-</div>
 
 </template>
 
 <script>
 export default {
     name: 'Main',
-
-    data() {
-        return {
-            posts:[],
-            currentPage: 1
-        };
-    },
-
-    methods: {
-        getPosts(apiPage) {
-            axios.get('/api/posts', {
-                'params': {
-                    'page': apiPage
-                }
-            })
-            .then((response) => {
-                this.currentPage = response.data.results.current_page;
-                this.posts = response.data.results.data;
-                this.lastPage = response.data.results.last_page;
-            });
-           
-        },
-
-        
-    },
-    created() {
-        this.getPosts();
-
-    }
-
-
 }
 </script>
 
--- resources/js/front.js
+++ resources/js/front.js
@@ -5,8 +5,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import Vue from 'vue';
 import App from './views/App';
 
+//import per usare vue-router
+
+import router from './router';
+
 const app = new Vue({
 
     el:'#root',
-    render: h => h(App)
+    render: h => h(App),
+    router
 });
